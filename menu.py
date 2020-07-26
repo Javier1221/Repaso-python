065aec46c3595a06f06a27997e57d8ade73fb230
Javier Marin <gonzalezamistad0220@gmail.com>
2020-07-26 01:49:00 -0500

    Primera version

---
+++ menu.py
@@ -0,0 +1,55 @@
+import procedimientos as llamado
+import os
+if __name__ == "__main__":
+    menu=0
+    while(menu!=11):
+        print("\n\n")
+        print("          ----Problemas de Repaso----       ")
+        print("______________________________________________")
+        print("| 1-Repetición de Palabra 10 veces           |")
+        print("| 2-Numeros impares hasta el numero ingresado|")
+        print("| 3-Saludo Hola                              |")
+        print("| 4- Saludo con nombre                       |")
+        print("| 5-Area de círculo y cilindro               |")
+        print("| 6-Media de una lista de numeros            |")
+        print("| 7-Edad del usuario                         |")
+        print("| 8-Comparación de Contraseñas               |")
+        print("| 9-Division de numeros                      |")
+        print("| 10-Numero que es par o impar               |")
+        print("| 11-Salir                                   |")
+        print("______________________________________________")
+        menu=int(input("Ingrese el numero="))
+        if menu==1:
+            llamado.p1()
+        elif menu==2:
+            llamado.p2()
+        elif menu==3:
+            llamado.p3()
+        elif menu==4:
+            os.system("cls")
+            print(" Opción 4 ")
+            nombre=input("Ingrese un nombre : ")
+            llamado.p4(nombre)
+        elif menu==5:
+            llamado.p5()
+        elif menu==6:
+            print(" Opción 6 ")
+            os.system("cls")
+            cantidad=int(input("Ingrese la cantidad de numeros que agregara en la lista"))
+            lista=[]
+            for i in range(0,cantidad):
+                numero=int(input("Ingrese el valor "+str(i+1)+" de la lista :"))
+                lista.append(numero)
+            llamado.p6(lista)
+        elif menu==7:
+            llamado.p7()
+        elif menu==8:
+            #La contraseña es hola mundo
+            llamado.p8()
+        elif menu==9:
+            llamado.p9()
+        elif menu==10:
+            llamado.p10()
+        elif menu==11:
+            os.system("cls")
+            print("Gracias por Paricipar")
---
+++ procedimientos.py
@@ -0,0 +1,100 @@
+import math
+import os
+def p1():
+    os.system("cls")
+    print(" Opción 1 ")
+    palabra=""
+    palabra=input("Ingrese una palabra ")
+    for i in range(0,10):
+        print(palabra)
+#///////////////////////////////////////////////////
+def p2():
+    os.system("cls")
+    print(" Opción 2 ")
+    numero=0
+    numero=int(input("Ingrese un número entero positivo: "))
+    for i in range(1,numero+1):
+        if i%2!=0:
+            print(i)
+#///////////////////////////////////////////////////
+def p3():
+    os.system("cls")
+    print(" Opción 3 ")
+    print("!Hola Amiga!")
+#///////////////////////////////////////////////////
+def p4(nombre):
+    print("Hola "+nombre)
+
+#///////////////////////////////////////////////////
+def p5():
+    os.system("cls")
+    print(" Opción 5 ")
+    radio=0
+    altura=0
+    area_cilindro=0
+    area_circulo=0
+    radio=float(input("Ingrese el radio: "))
+    altura=float(input("Ingrese la altura: "))
+    area_circulo= math.pi * (radio**2)
+    print("El area del círculo  es:{:.2f}".format(area_circulo))
+    area_cilindro=2*math.pi*radio*(radio+altura)
+    print("El area del cílindro  es:{:.2f}".format(area_cilindro))
+#///////////////////////////////////////////////////
+def p6(lista_numeros):
+    sumatoria=0
+    media=0
+    for i in range(0,len(lista_numeros)):
+        sumatoria+=lista_numeros[i]
+    media=sumatoria/len(lista_numeros)
+    print("La media de los datos ingresados es :{:.2f}".format(media))
+
+
+#///////////////////////////////////////////////////
+def p7():
+    os.system("cls")
+    print(" Opción 7 ")
+    edad=0
+    edad=int(input("Por favor ingrese su edad "))
+    if edad<18:
+        print(" Oh eres menor de edad")
+    else:
+        print("Oh que bien ya eres mayor de edad ")
+#///////////////////////////////////////////////////
+def p8():
+    os.system("cls")
+    print(" Opción 8 ")
+    contraseña="hola mundo"
+    contraseña_usuario=""
+    contraseña_usuario=input("Ingrese la contraseña ")
+    contraseña_usuario=contraseña_usuario.lower()
+    if(contraseña==contraseña_usuario):
+        print("Contraseña correcta ")
+    else:
+        print("Contraseña incorrecta ")
+
+#///////////////////////////////////////////////////
+def p9():
+    os.system("cls")
+    print(" Opción 9 ")
+    validacion=False
+    while(validacion != True):
+        numerador=float(input("Ingrese el numerador :"))
+        denominador=float(input("Ingrese el denominador :"))
+        try:
+            division=numerador/denominador
+            print("La división es :{:.2f}".format(division))
+            validacion=True
+        except ZeroDivisionError:
+            print("Ups parece que tu divisor es 0...")
+            print("Nuevamente ")
+            validacion=False
+#///////////////////////////////////////////////////
+def p10():
+    os.system("cls")
+    print(" Opción 10 ")
+    numero=int(input("Ingrese un numero entero "))
+    if numero%2==0:
+        print("El número ingresado es par")
+    else:
+        print("El número ingresado es impar")
+
